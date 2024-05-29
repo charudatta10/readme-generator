e9626cc67a49d6dd3feba1a521ae6ec625a8fc35
charudatta <152109007c@gmail.com>
2024-05-29 14:23:18 +0530

    refactored code

--- readme_generator.py
+++ readme_generator.py
@@ -1,124 +1,59 @@
 import random
 import json
 
-with open("config.json", "r") as f:
-    data = json.load(f)
 
-def ufunc_lst2str(inpt_list):
-    inpt_list = ["- " + item + "\n" for item in inpt_list]
-    opt_str = "".join(inpt_list)
-    return opt_str
+class readme_gen():
 
-def ufunc_dict2str(inpt_dict):
-    opt_str = ""
-    for (key, value) in inpt_dict.items():
-        t_str = f"""> {key}?    \n   {value}.    \n   \n"""
-        opt_str += t_str
-    return opt_str
+    def add_config(self, file):
+        with open(file , "r") as f:
+            self.data = json.load(f)
 
-def ufunc_badgegen(list_badges):
-    tlist_badgegen = []
-    for i in list_badges:
-        badge_label = i.capitalize()
-        badge_logo = i
-        badge_color = ''.join([random.choice('ABCDEF0123456789') for _ in range(6)])
-        logo_color =  '000' if int(str(badge_color),16) > int(str('7FFFFF'),16) else 'fff'
-        _badgegen = f"![](https://img.shields.io/badge/{badge_label}-{badge_color}?style=for-the-badge&logo={badge_logo}&logoColor={logo_color})"
-        tlist_badgegen.append(_badgegen+" ")
-    tstr_badgen = "".join(tlist_badgegen)
-    return tstr_badgen
+    def ufunc_lst2str(self, inpt_list):
+        inpt_list = ["- " + item + "\n" for item in inpt_list]
+        opt_str = "".join(inpt_list)
+        return opt_str
 
-doc = f""" 
-<!-- PROJECT Banner ![Readme](./Designer%20(3).png)-->
-![SVG Banners](https://svg-banners.vercel.app/api?type=luminance&text1={data['title']}&width=1020&height=460)
-<!-- PROJECT TITLE --> <!-- <a name="readme-top"></a> -->
-# {data['title']} <!-- PROJECT LOGO ![Readme](./Designer%20(3).png) -->
+    def ufunc_dict2str(self, inpt_dict):
+        opt_str = ""
+        for (key, value) in inpt_dict.items():
+            t_str = f"""> {key}?    \n   {value}.    \n   \n"""
+            opt_str += t_str
+        return opt_str
 
-<!-- PROJECT SHIELDS -->
-![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/charudatta10/{data['title']}?include_prereleases)
-![GitHub last commit](https://img.shields.io/github/last-commit/charudatta10/{data['title']})
-![GitHub issues](https://img.shields.io/github/issues-raw/charudatta10/{data['title']})
-![GitHub pull requests](https://img.shields.io/github/issues-pr/charudatta10/{data['title']})
-![GitHub](https://img.shields.io/github/license/charudatta10/{data['title']})
+    def ufunc_badgegen(self, list_badges):
+        tlist_badgegen = []
+        for i in list_badges:
+            badge_label = i.capitalize()
+            badge_logo = i
+            badge_color = ''.join([random.choice('ABCDEF0123456789') for _ in range(6)])
+            logo_color =  '000' if int(str(badge_color),16) > int(str('7FFFFF'),16) else 'fff'
+            _badgegen = f"![](https://img.shields.io/badge/{badge_label}-{badge_color}?style=for-the-badge&logo={badge_logo}&logoColor={logo_color})"
+            tlist_badgegen.append(_badgegen+" ")
+        tstr_badgen = "".join(tlist_badgegen)
+        return tstr_badgen
 
-<!-- Project Description -->
-{data['description']}  
+    def gen_str(self):
+        with open("template.md", "r", encoding="utf-8") as f:
+            tdoc = f.read()
+        print(self.data)
+        self.doc = tdoc.format(title = self.data['title'],
+                          description = self.data['description'],
+                          features = self.ufunc_lst2str(self.data['features']),
+                          list_badges = self.ufunc_badgegen(self.data['list_badges']),
+                          dependencies = self.ufunc_lst2str(self.data['dependencies']),
+                          steps = self.ufunc_lst2str(self.data['steps']),
+                          FAQ = self.ufunc_dict2str(self.data['FAQ']),
+                     )
 
-<!-- SHARING ON SOCIAL MEDIA -->
+        print(self.doc)
 
-<!-- TABLE OF CONTENTS -->
-
-## Project Preview 📖 <!-- Usage screenshots -->
-
-![Screenshot](Screenshot.png)
-
-<!-- <p align="right"><a href="#readme-top">Jump to Top<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand gestures/Index Pointing Up.png" alt="Pointing Up" width="25" height="25" /></a></p>
--->
-
-## Features 🌟
-
-{ ufunc_lst2str(data['features'])}
-
-## Built With 🔧
-{ufunc_badgegen(data['list_badges'])}
-
-<!-- GETTING STARTED -->
-
-## Getting Started 🌱
-
-### Dependencies ⧉
-
-{ ufunc_lst2str(data['dependencies'])}
-
-### Installation ■■■■■■■■■■
-
-1. To install from binaries:  
-   Download the binary file from the release and double-click to use it.
-
-2. To install from source:
-
-```PowerShell
-gh repo clone charudatta10/{data['title']}
-```
- 
-### How to use 🗎
-
-{ ufunc_lst2str(data['steps'])}
-
-![Screenshot](carbon.png)
-
-
-## FAQ ?
-
-✨[Report a 🐛 or Request a ⭐](https://github.com/charudatta10/{data['title']}/issues)✨
-
-{ufunc_dict2str(data['FAQ'])}
-
-<!-- CONTRIBUTING -->
-
-## Authors 👱
-
-The author of this project is @charudatta10.  
-
-## Contributors 👪
-
-| ![](profile-picture.png) |
-| :---: | 
-| Charudatta Korde |
-| [💻](#code-charudatta10)  [📖](#doc-charudatta10)  [🎨](#design-charudatta10)  [💡](#example-charudatta10)  [🤔](#ideas-charudatta10)|
-
-
-### Contribution guidelines
-
-The contribution to this project should adhere to GPL-3.0 and respect the copyright claims of charudatta10.
-
-## License 📜
-
-Copyright :copyright: 2024 ![ck](favicon05.svg):tm: @ charudatta10.   
-The project is licensed [GPL-3.0](./LICENSE).
-
-"""
-
-with open("README.md", "w", encoding="utf-8") as f:
-    f.write(doc)
+    def gen_file(self):
+        with open("README.md", "w", encoding="utf-8") as f:
+            f.write(self.doc)
 # print(doc)
+
+if __name__ == "__main__":
+    file = readme_gen()
+    file.add_config("config.json")
+    file.gen_str()
+    #file.gen_file("README.md")
---
+++ template.md
@@ -0,0 +1,88 @@
+ 
+<!-- PROJECT Banner ![Readme](./Designer%20(3).png)-->
+![SVG Banners](https://svg-banners.vercel.app/api?type=luminance&text1={title}&width=1020&height=460)
+<!-- PROJECT TITLE --> <!-- <a name="readme-top"></a> -->
+# {title} <!-- PROJECT LOGO ![Readme](./Designer%20(3).png) -->
+
+<!-- PROJECT SHIELDS -->
+![GitHub release (latest by date including pre-releases)](https://img.shields.io/github/v/release/charudatta10/{title}?include_prereleases)
+![GitHub last commit](https://img.shields.io/github/last-commit/charudatta10/{title})
+![GitHub issues](https://img.shields.io/github/issues-raw/charudatta10/{title})
+![GitHub pull requests](https://img.shields.io/github/issues-pr/charudatta10/{title})
+![GitHub](https://img.shields.io/github/license/charudatta10/{title})
+
+<!-- Project Description -->
+{description}  
+
+<!-- SHARING ON SOCIAL MEDIA -->
+
+<!-- TABLE OF CONTENTS -->
+
+## Project Preview 📖 <!-- Usage screenshots -->
+
+![Screenshot](Screenshot.png)
+
+<!-- <p align="right"><a href="#readme-top">Jump to Top<img src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Hand gestures/Index Pointing Up.png" alt="Pointing Up" width="25" height="25" /></a></p>
+-->
+
+## Features 🌟
+
+{features} 
+
+## Built With 🔧
+{list_badges}
+
+<!-- GETTING STARTED -->
+
+## Getting Started 🌱
+
+### Dependencies ⧉
+
+{dependencies}
+
+### Installation ■■■■■■■■■■
+
+1. To install from binaries:  
+   Download the binary file from the release and double-click to use it.
+
+2. To install from source:
+
+```PowerShell
+gh repo clone charudatta10/{title}
+```
+ 
+### How to use 🗎
+
+{steps}
+
+![Screenshot](carbon.png)
+
+
+## FAQ ?
+
+✨[Report a 🐛 or Request a ⭐](https://github.com/charudatta10/{title}/issues)✨
+
+{FAQ}
+
+<!-- CONTRIBUTING -->
+
+## Authors 👱
+
+The author of this project is @charudatta10.  
+
+## Contributors 👪
+
+| ![](profile-picture.png) |
+| :---: | 
+| Charudatta Korde |
+| [💻](#code-charudatta10)  [📖](#doc-charudatta10)  [🎨](#design-charudatta10)  [💡](#example-charudatta10)  [🤔](#ideas-charudatta10)|
+
+
+### Contribution guidelines
+
+The contribution to this project should adhere to GPL-3.0 and respect the copyright claims of charudatta10.
+
+## License 📜
+
+Copyright :copyright: 2024 ![ck](favicon05.svg):tm: @ charudatta10.   
+The project is licensed [GPL-3.0](./LICENSE).
