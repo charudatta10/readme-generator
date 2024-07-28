fc85b7f55b09d3a5e847ef5b263de4fd12f19928
charudatta <152109007c@gmail.com>
2024-07-28 19:00:01 +0530

    modified test case

--- tests/test_readmegen.py
+++ tests/test_readmegen.py
@@ -4,24 +4,21 @@ from pathlib import Path
 
 class TestReadmeGen(unittest.TestCase):
     def setUp(self):
-        self.readme = ReadmeGen()  
+        self.readme = ReadmeGen() 
+        self.file_name = "tests/test_README.md"
+        self.readme.main()
 
     def test_add_template(self):
-        self.readme.add_template()
         self.assertEqual(self.readme.template_path, Path(__file__).parent.parent / "src/template.md")
         self.assertTrue(hasattr(self.readme, 'template'), "Template doesn't exist.")
 
     def test_add_config(self):
-        self.readme.add_config()
         self.assertTrue(hasattr(self.readme, 'data'), "Config is missing.")
 
     def test_gen_str(self):
-        self.readme.gen_str()
         self.assertTrue(hasattr(self.readme, 'doc'), "Documentation did not generated.")
 
     def test_gen_file(self):
-        self.file_name = "tests/test_README.md"
-        self.readme.gen_file()
         self.assertTrue(Path("README.md").exists(), f"README did not generated.")
 
 # Run the tests
