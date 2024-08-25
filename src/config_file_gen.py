044774f8f3bc1256578e8bf0a0ead91887f11cd7
charudatta <152109007c@gmail.com>
2024-08-25 23:47:48 +0530

    changed to sheildio

--- src/config_file_gen.py
+++ src/config_file_gen.py
@@ -27,12 +27,8 @@ class ConfigGen():
         for i in list_badges:
             badge_label = i.capitalize()
             badge_logo = i
-            badge_color = "#314042"
-            #logo_color = (
-            #    "000" if int(str(badge_color), 16) > int(str("7FFFFF"), 16) else "fff"
-            #)
-            #_badgegen = f"![](https://img.shields.io/badge/{badge_label}-#263759?style=for-the-badge&logo={badge_logo}&logoColor={logo_color})"
-            _badgegen=f"![](https://badgen.net/badge/%20/{badge_label}/blue?icon={badge_logo})"
+            _badgegen = f"![](https://img.shields.io/badge/{badge_label}-#263759?style=for-the-badge&logo={badge_logo}&logoColor={logo_color})"
+            #_badgegen=f"![](https://badgen.net/badge/%20/{badge_label}/blue?icon={badge_logo})"
             tlist_badgegen.append(_badgegen + " ")
         tstr_badgen = "".join(tlist_badgegen)
         #modify
